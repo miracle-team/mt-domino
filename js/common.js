cb9223618da08c253b0dd74c332eba9124e9e118
Secure-kos <secure-kos@yandex.ru>
2017-04-19 10:11:07 +0300

    Final

--- js/common.js
+++ js/common.js
@@ -13,6 +13,47 @@ $(document).ready(function() {
     speed: 300,
     dots: true
   });
+  
+  $('.carousel').slick({
+    dots: true,
+    appendArrows: '.carousel-arrows .container'
+  });
+  
+  $('.rating-carousel').slick({
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToScroll: 1,
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+  })
+  
+  //Fixed nav
+  
+  $(window).scroll(function() {
+    
+    var nav_top = $('.header-wrap').height();
+    var nav_height = $('.nav-wrap').height();
+    if( $(this).scrollTop() >= nav_top ) {
+      $('.nav-wrap').addClass('fixed');
+      $('body').css({"padding-top": nav_height});
+    }
+    else {
+      $('.nav-wrap').removeClass('fixed');
+      $('body').css({"padding-top": "0"});
+    }
+  });
 
 });
 
